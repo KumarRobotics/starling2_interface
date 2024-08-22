df036265f77ee0e7298379ca367d8da3c89dcef3
Kashish Garg <kashishgarg9@gmail.com>
2024-08-22 16:40:40 -0400

    Cleaned up ground control script

--- scripts/starling2_ground_control.sh
+++ scripts/starling2_ground_control.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# ORDER of OPERATIONS:
+# 1. Control Operator starts the ./flight_start script on Starling2
+# 2. Control Operator hits enter on system.launch window
+# 3. Safety Pilot arms motors using RC
+# 4. Control Operator send mavros arming command, waits for Success to be True
+# 5. Control Operator hits Motors on in RQT_MAV_MANAGER
+# 6. Control Operator counts down and hits Takeoff, Safety Pilot should go to OFFBOARD right AFTER Takeoff is hit!
+
 clear
 session="starling_ground"
 
@@ -23,6 +31,7 @@ window=0
 tmux send-keys -t $session:$window 'bash' C-m 
 tmux send-keys -t $session:$window 'clear' C-m 
 tmux send-keys -t $session:$window 'ssh root@'"${ip_star}" C-m
+
 tmux split-window -h  
 tmux select-pane -t 1
 tmux send-keys -t $session:$window 'distrobox enter noetic' C-m 
@@ -32,7 +41,9 @@ tmux send-keys -t $session:$window 'export ROS_IP='"${ip_add}" C-m
 tmux send-keys -t $session:$window 'export ROS_HOSTNAME=ros' C-m 
 tmux send-keys -t $session:$window 'export ROS_MASTER_URI=http://'"${ip_star}"':11311' C-m 
 tmux send-keys -t $session:$window 'source ~/voxl2_home/autonomy_ws/devel/setup.bash' C-m 
-tmux send-keys -t $session:$window 'python3 ~/voxl2_home/autonomy_ws/src/kr_mav_control/rqt_mav_manager/scripts/rqt_mav_manager' 
+
+# RUN THIS COMMAND FIRST AND ENSURE SUCCESS IS TRUE
+tmux send-keys -t $session:$window 'rosservice call /mavros/cmd/arming "value: true"'  
 
 tmux split-window -v  
 tmux select-pane -t 2
@@ -43,7 +54,10 @@ tmux send-keys -t $session:$window 'export ROS_IP='"${ip_add}" C-m
 tmux send-keys -t $session:$window 'export ROS_HOSTNAME=ros' C-m 
 tmux send-keys -t $session:$window 'export ROS_MASTER_URI=http://'"${ip_star}"':11311' C-m 
 tmux send-keys -t $session:$window 'source ~/voxl2_home/autonomy_ws/devel/setup.bash' C-m 
-tmux send-keys -t $session:$window 'rosservice call /mavros/cmd/arming "value: true"'  
+
+# HIT MOTORS ON THEN TAKEOFF, GO TO OFFBOARD AFTER TAKEOFF, GODSPEED QUADROTOR 
+tmux send-keys -t $session:$window 'python3 ~/voxl2_home/autonomy_ws/src/kr_mav_control/rqt_mav_manager/scripts/rqt_mav_manager' 
+
 
 window=1
 tmux new-window -t $session -n "kill"
